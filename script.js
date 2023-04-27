bc778c2420811ebf6616770434b89eb465e699aa
HFTF <42843423+Mark-HFTF@users.noreply.github.com>
2023-04-27 14:48:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function handleClick(e) {
   }
   if (isBoardFull()) {
     setTimeout(() => {
-      alert('It\'s a draw!');
+      alert("It's a draw!");
       resetGame();
     }, 100);
     return;
@@ -30,4 +30,28 @@ function checkWin(player) {
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
-    [1, 4
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  return winConditions.some((combination) =>
+    combination.every((index) => cells[index].getAttribute('data-mark') === player),
+  );
+}
+
+function isBoardFull() {
+  return [...cells].every((cell) => cell.getAttribute('data-mark'));
+}
+
+function resetGame() {
+  currentPlayer = 'X';
+  cells.forEach((cell) => {
+    cell.textContent = '';
+    cell.removeAttribute('data-mark');
+  });
+}
+
+cells.forEach((cell) => cell.addEventListener('click', handleClick));
+restartButton.addEventListener('click', resetGame);
